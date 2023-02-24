45b71c2f106198d65cd9568cfe598c6de63e1bee
Mateus Paz <54851249+mateusflorez@users.noreply.github.com>
2023-02-24 10:32:15 -0300

    🚧 create user table

---
+++ server/prisma/migrations/20230224133036_created_user_table/migration.sql
@@ -0,0 +1,22 @@
+-- CreateTable
+CREATE TABLE "user" (
+    "id" TEXT NOT NULL,
+    "username" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "level" INTEGER NOT NULL,
+    "profession" TEXT NOT NULL,
+    "experience" INTEGER NOT NULL,
+    "gold" INTEGER NOT NULL,
+    "completed_missions" INTEGER NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "user_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "user_username_key" ON "user"("username");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "user_email_key" ON "user"("email");
