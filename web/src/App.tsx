85f74d23fa56e363f01ada16acdb4f41c2aa2a94
Mateus Paz <54851249+mateusflorez@users.noreply.github.com>
2023-02-24 12:10:50 -0300

    ✨ login page working

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,11 +1,13 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Register from './pages/Register'
+import Login from './pages/Login'
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
       </Routes>
     </BrowserRouter>
   )
